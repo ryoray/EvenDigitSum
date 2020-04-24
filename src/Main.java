cfda4c5780e26a38f72e16ee565ad1d0932d3c79
ryoray <praretno.wibowo@gmail.com>
2020-04-24 11:08:21 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(EvenDigitSum.getEvenDigitSum(123456789));    // 20
+        System.out.println(EvenDigitSum.getEvenDigitSum(252));          // 4
+        System.out.println(EvenDigitSum.getEvenDigitSum(-22));          // -1
+    }
+}
